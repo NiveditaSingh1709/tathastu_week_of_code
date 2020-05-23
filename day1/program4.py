57452ae100585519647159ad392ab1034a4256e8
NiveditaSingh1709 <65765244+NiveditaSingh1709@users.noreply.github.com>
2020-05-23 13:44:04 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,6 @@
+costprice=int(input("enter the cost price"))
+sellingprice =int(input("enter the selling price"))
+profit=(sellingprice-costprice)
+print(profit,"profit")
+newsellingprice=1.05*profit+costprice
+print(newsellingprice,"newsellingprice")
